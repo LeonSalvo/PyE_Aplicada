dfe2fa88ca09ce079038737b9f744bf688f22b41
LeonSalvo <110829744+LeonSalvo@users.noreply.github.com>
2024-06-08 20:43:43 -0300

    mejores prints

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,11 +2,13 @@ import numpy as np
 from scipy.stats import binom, mode, geom, poisson
 import matplotlib.pyplot as plt
 
+
 # Distribución 1 - binomial
 
 def binomial_distribution(n, p, sizes):
     samples = [binom.rvs(n, p, size=size) for size in sizes]
     return samples, sizes
+
 
 def first_distribution(n, p, sizes):
     samples, sizes = binomial_distribution(n, p, sizes)
@@ -21,11 +23,13 @@ def first_distribution(n, p, sizes):
     print(f"Esperanza teórica de la distribución binomial: {theoretical_mean}")
     print(f"Varianza teórica de la distribución binomial: {theoretical_variance}")
 
+
 # Distribución 2 - geométrica
 
 def geometrical_distribution(p, sizes):
     samples = [geom.rvs(p, size=size) for size in sizes]
     return samples, sizes
+
 
 def second_distribution(p, sizes):
     samples, sizes = geometrical_distribution(p, sizes)
@@ -40,11 +44,13 @@ def second_distribution(p, sizes):
     print(f"Esperanza teórica de la distribución geométrica: {theoretical_mean}")
     print(f"Varianza teórica de la distribución geométrica: {theoretical_variance}")
 
+
 # Distribución 3 - Poisson
 
 def poisson_distribution(l, sizes):
     samples = [poisson.rvs(l, size=size) for size in sizes]
     return samples, sizes
+
 
 def third_distribution(l, sizes):
     samples, sizes = poisson_distribution(l, sizes)
@@ -58,28 +64,37 @@ def third_distribution(l, sizes):
     theoretical_variance = l
     print(f"Esperanza teórica de la distribución de Poisson: {theoretical_mean}")
     print(f"Varianza teórica de la distribución de Poisson: {theoretical_variance}")
-    
+
+
 # Funciones auxiliares
 
 def calc_mode(samples, sizes):
     for i, sample in enumerate(samples):
         mode_val = mode(sample)[0]
         print(f"Muestra de tamaño {sizes[i]}: Moda = {mode_val}")
+    print ("\n")
+
 
 def calc_median(samples, sizes):
     for i, sample in enumerate(samples):
         median = np.median(sample)
         print(f"Muestra de tamaño {sizes[i]}: Mediana = {median}")
+    print ("\n")
+
 
 def calc_mean(samples, sizes):
     for i, sample in enumerate(samples):
         mean = np.mean(sample)
         print(f"Muestra de tamaño {sizes[i]}: Media empírica = {mean}")
+    print ("\n")
+
 
 def calc_variance(samples, sizes):
     for i, sample in enumerate(samples):
         variance = np.var(sample, ddof=1)  # ddof=1 para obtener la varianza muestral
         print(f"Muestra de tamaño {sizes[i]}: Varianza empírica = {variance}")
+    print ("\n")
+
 
 def create_boxplots(samples, sizes):
     plt.figure(figsize=(8, 4))
@@ -93,6 +108,7 @@ def create_boxplots(samples, sizes):
     plt.tight_layout()
     plt.show()
 
+
 def create_histograms(samples, sizes):
     plt.figure(figsize=(8, 4))
     for i, sample in enumerate(samples):
@@ -104,15 +120,17 @@ def create_histograms(samples, sizes):
     plt.tight_layout()
     plt.show()
 
+
 # Función principal
 
 def main():
-    sizes = [10**2, 10**3, 10**4, 10**5]
-    print("Distribución binomial")
+    sizes = [10 ** 2, 10 ** 3, 10 ** 4, 10 ** 5]
+    print("\n-------------Distribución binomial-------------\n")
     first_distribution(100, 0.35, sizes)
-    print("Distribución geométrica")
+    print("\n-------------Distribución geométrica-------------\n")
     second_distribution(0.3, sizes)
-    print("Distribución de Poisson")
+    print("\n-------------Distribución de Poisson-------------\n")
     third_distribution(30, sizes)
+
 
 main()
